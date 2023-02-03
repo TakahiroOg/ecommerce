fbb08e159cfa6ad31ec866b0dde4825d8544456a
Takahiro <boroney1@gmail.com>
2023-02-04 00:56:47 +0900

    setup

---
+++ backend/base/products.py
@@ -0,0 +1,62 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'Airpods',
+    'image': '/images/1.jpg',
+    'description': 'Bluetooth technology lets you connect...',
+    'brand': 'Apple',
+    'category': 'electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12
+  },
+  {
+    '_id': '2',
+    'name': 'Airpods',
+    'image': '/images/2.jpg',
+    'description': 'Bluetooth technology lets you connect...',
+    'brand': 'Apple',
+    'category': 'electronics',
+    'price': 89.99,
+    'countInStock': 0,
+    'rating': 4.5,
+    'numReviews': 12
+  },
+  {
+    '_id': '3',
+    'name': 'Airpods',
+    'image': '/images/3.jpg',
+    'description': 'Bluetooth technology lets you connect...',
+    'brand': 'Apple',
+    'category': 'electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12
+  },
+  {
+    '_id': '4',
+    'name': 'Airpods',
+    'image': '/images/4.jpg',
+    'description': 'Bluetooth technology lets you connect...',
+    'brand': 'Apple',
+    'category': 'electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12
+  },
+  {
+    '_id': '5',
+    'name': 'Airpods',
+    'image': '/images/5.jpg',
+    'description': 'Bluetooth technology lets you connect...',
+    'brand': 'Apple',
+    'category': 'electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12
+  },
+]
